45920b7a1b7a1bc663d06683d5a32299c64018ac
myrrlyn <self@myrrlyn.dev>
2019-09-18 08:35:07 -0600

    Make the traits generic over the underlying type, rather than specific to it

--- radium/src/lib.rs
+++ radium/src/lib.rs
@@ -1,7 +1,6 @@
 //! `radium` provides a series of helper traits providing a uniform API for
 //! interacting with both atomic types like
-//! [`AtomicUsize`](core::sync::atomic::AtomicUsize), and non-atomic types like
-//! [`Cell<usize>`](core::cell::Cell).
+//! [`AtomicUsize`], and non-atomic types like [`Cell<T>`].
 //!
 //! This crate is `#![no_std]`-compatible, and uses no non-core types.
 //!
@@ -18,12 +17,16 @@
 //! > no but call the crate radium
 //! >
 //! > (since people didn't care that it was radioactive and used it in everything)
+//!
+//! [`AtomicUsize`]: core::sync::atomic::AtomicUsize
+//! [`Cell<T>`]: core::cell::Cell
 
 #![no_std]
 #![deny(unconditional_recursion)]
 
 use core::cell::Cell;
 use core::sync::atomic::{
+    self,
     fence, AtomicBool, AtomicI16, AtomicI32, AtomicI64, AtomicI8, AtomicIsize, AtomicPtr,
     AtomicU16, AtomicU32, AtomicU64, AtomicU8, AtomicUsize, Ordering,
 };
@@ -395,10 +398,14 @@ macro_rules! cell_impl {
             &self,
             current: $T,
             new: $T,
-            success: Ordering,
-            failure: Ordering,
+            _success: Ordering,
+            _failure: Ordering,
         ) -> Result<$T, $T> {
-            self.compare_exchange(current, new, success, failure)
+            if self.get() == current {
+                Ok(self.replace(new))
+            } else {
+                Err(self.get())
+            }
         }
     };
 
@@ -449,15 +456,18 @@ macro_rules! radium_int {
 This trait is implemented by both [`", stringify!($Atomic), "`] and
 [`Cell<", stringify!($T), ">`](Cell), providing a consistent
 interface for interacting with the two types."),
+            #[deprecated(since = "0.2", note = "Use `Radium<T>` instead")]
             pub trait $Radium {
                 trait_decl!(int: $T, stringify!($Atomic));
             }
         }
 
+        #[allow(deprecated)]
         impl $Radium for $Atomic {
             atomic_impl!(int: $T);
         }
 
+        #[allow(deprecated)]
         impl $Radium for Cell<$T> {
             cell_impl!(int: $T);
         }
@@ -480,14 +490,17 @@ radium_int!(RadiumUsize, AtomicUsize, usize);
 ///
 /// This trait is implemented by both [`AtomicBool`] and [`Cell<bool>`](Cell),
 /// providing a consistent interface for interacting with the two types.
+#[deprecated(since = "0.2", note = "Prefer `Radium<bool>` instead")]
 pub trait RadiumBool {
     trait_decl!(binary: bool, "AtomicBool");
 }
 
+#[allow(deprecated)]
 impl RadiumBool for AtomicBool {
     atomic_impl!(binary: bool);
 }
 
+#[allow(deprecated)]
 impl RadiumBool for Cell<bool> {
     cell_impl!(binary: bool);
 }
@@ -499,14 +512,465 @@ impl RadiumBool for Cell<bool> {
 /// with the two types.
 ///
 /// [`*mut T`]: https://doc.rust-lang.org/stable/std/primitive.pointer.html
+#[deprecated(since = "0.2", note = "Prefer `Radium<*mut T>` instead")]
 pub trait RadiumPtr<T> {
     trait_decl!(base: *mut T, "AtomicPtr");
 }
 
+#[allow(deprecated)]
 impl<T> RadiumPtr<T> for AtomicPtr<T> {
     atomic_impl!(base: *mut T);
 }
 
+#[allow(deprecated)]
 impl<T> RadiumPtr<T> for Cell<*mut T> {
     cell_impl!(base: *mut T);
 }
+
+/** A maybe-atomic shared mutable fundamental type `T`.
+
+This trait is implemented by both the [atomic wrapper] type for `T`, and by
+[`Cell<T>`], providing a consistent interface for interacting with the two
+types.
+
+This trait only provides atomic load/store access, and does not perform
+type-specific operations on the values. For types that can be used in bitwise
+arithmetic, see [`RadiumBits`]; for types that can be used in integer
+arithmetic, see [`RadiumMath`].
+
+[atomic wrapper]: core::sync::atomic
+[`Cell<T>`]: core::cell::Cell
+[`RadiumBits`]: crate::RadiumBits
+[`RadiumMath`]: crate::RadiumMath
+**/
+pub trait Radium<Item> {
+    /// Creates a new value of this type.
+    fn new(value: Item) -> Self;
+
+    /// If the underlying value is atomic, calls [`fence`] with the given
+    /// [`Ordering`]. Otherwise, does nothing.
+    ///
+    /// [`Ordering`]: core::sync::atomic::Ordering
+    /// [`fence`]: core::sync::atomic::fence
+    fn fence(order: Ordering);
+
+    /// Returns a mutable reference to the underlying value.
+    ///
+    /// This is safe because the mutable reference to `self` guarantees that no
+    /// other references exist to this value.
+    fn get_mut(&mut self) -> &mut Item;
+
+    /// Consumes the wrapper and returns the contained value.
+    ///
+    /// This is safe as passing by value ensures no other references exist.
+    fn into_inner(self) -> Item;
+
+    /// Load a value from this object.
+    ///
+    /// Ordering values are ignored by non-atomic types.
+    ///
+    /// See also: [`AtomicU8::load`].
+    ///
+    /// [`AtomicU8::load`]: core::sync::atomic::AtomicU8::load
+    fn load(&self, order: Ordering) -> Item;
+
+    /// Store a value in this object.
+    ///
+    /// Ordering arguments are ignored by non-atomic types.
+    ///
+    /// See also: [`AtomicU8::store`]: core::sync::atomic::AtomicU8::store
+    fn store(&self, value: Item, order: Ordering);
+
+    /// Swap with the value stored in this object.
+    ///
+    /// Ordering arguments are ignored by non-atomic types.
+    ///
+    /// See also: [`AtomicU8::swap`].
+    ///
+    /// [`AtomicU8::swap`]: core::sync::atomic::AtomicU8::swap
+    fn swap(&self, value: Item, order: Ordering) -> Item;
+
+    /// Stores a value into this object if the currently-stored value is the
+    /// same as the `current` value.
+    ///
+    /// The return value is always the previously-stored value. If it is equal to
+    /// `current`, then the value was updated with `new`.
+    ///
+    /// Ordering arguments are ignored by non-atomic types.
+    ///
+    /// See also: [`AtomicU8::compare_and_swap`].
+    ///
+    /// [`AtomicU8::compare_and_swap`]: core::sync::atomic::AtomicU8::compare_and_swap
+    fn compare_and_swap(
+        &self,
+        current: Item,
+        new: Item,
+        order: Ordering,
+    ) -> Item;
+
+    /// Stores a value into this object if the currently-stored value is the
+    /// same as the `current` value.
+    ///
+    /// The return value is a `Result` indicating whether the new value was
+    /// written, and containing the previously-stored value. On success, this
+    /// value is guaranteed to be equal to `current`.
+    ///
+    /// Ordering arguments are ignored by non-atomic types.
+    ///
+    /// See also: [`AtomicU8::compare_exchange`].
+    ///
+    /// [`AtomicU8::compare_exchange`]: core::sync::atomic::AtomicU8::compare_exchange
+    fn compare_exchange(
+        &self,
+        current: Item,
+        new: Item,
+        success: Ordering,
+        failure: Ordering,
+    ) -> Result<Item, Item>;
+
+    /// Stores a value into this object if the currently-stored value is the
+    /// same as the `current` value.
+    ///
+    /// Unlike `compare_exchange`, this function is allowed to spuriously fail
+    /// even when the comparison succeeds, which can result in more efficient
+    /// code on some platforms. The return value is a `Result` indicating
+    /// whether the new value was written, and containing the previously-stored
+    /// value.
+    ///
+    /// Ordering arguments are ignored by non-atomic types.
+    ///
+    /// See also: [`AtomicU8::compare_exchange_weak`].
+    ///
+    /// [`AtomicU8::compare_exchange_weak`]: core::sync::atomic::AtomicU8::compare_exchange_weak
+    fn compare_exchange_weak(
+        &self,
+        current: Item,
+        new: Item,
+        success: Ordering,
+        failure: Ordering,
+    ) -> Result<Item, Item>;
+}
+
+/// Performs bitwise arithmetic on a maybe-atomic value.
+pub trait RadiumBits<Item>: Radium<Item> {
+    /// Performs a bitwise "and" on the currently-stored value and the argument
+    /// `value`, and stores the result in `self`.
+    ///
+    /// Returns the previously-stored value.
+    ///
+    /// Ordering arguments are ignored by non-atomic types.
+    ///
+    /// See also: [`AtomicU8::fetch_and`].
+    ///
+    /// [`AtomicU8::fetch_and`]: core::sync::atomic::AtomicU8::fetch_and
+    fn fetch_and(&self, value: Item, order: Ordering) -> Item;
+
+    /// Performs a bitwise "nand" on the currently-stored value and the argument
+    /// `value`, and stores the result in `self`.
+    ///
+    /// Returns the previously-stored value.
+    ///
+    /// Ordering arguments are ignored by non-atomic types.
+    ///
+    /// See also: [`AtomicU8::fetch_nand`].
+    ///
+    /// [`AtomicU8::fetch_nand`]: core::sync::atomic::AtomicU8::fetch_nand
+    fn fetch_nand(&self, value: Item, order: Ordering) -> Item;
+
+    /// Performs a bitwise "or" on the currently-stored value and the argument
+    /// `value`, and stores the result in `self`.
+    ///
+    /// Returns the previously-stored value.
+    ///
+    /// Ordering arguments are ignored by non-atomic types.
+    ///
+    /// See also: [`AtomicU8::fetch_or`].
+    ///
+    /// [`AtomicU8::fetch_or`]: core::sync::atomic::AtomicU8::fetch_or
+    fn fetch_or(&self, value: Item, order: Ordering) -> Item;
+
+    /// Performs a bitwise "xor" on the currently-stored value and the argument
+    /// `value`, and stores the result in `self`.
+    ///
+    /// Returns the previously-stored value.
+    ///
+    /// Ordering arguments are ignored by non-atomic types.
+    ///
+    /// See also: [`AtomicU8::fetch_xor`].
+    ///
+    /// [`AtomicU8::fetch_xor`]: core::sync::atomic::AtomicU8::fetch_xor
+    fn fetch_xor(&self, value: Item, order: Ordering) -> Item;
+}
+
+/// Performs integer arithmetic on a maybe-atomic value.
+pub trait RadiumMath<Item>: Radium<Item> {
+    /// Adds `value` to the currently-stored value, wrapping on overflow, and
+    /// stores the result in `self`.
+    ///
+    /// Returns the previously-stored value.
+    ///
+    /// Ordering arguments are ignored by non-atomic types.
+    ///
+    /// See also: [`AtomicU8::fetch_add`].
+    ///
+    /// [`AtomicU8::fetch_add`]: core::sync::atomic::AtomicU8::fetch_add
+    fn fetch_add(&self, value: Item, order: Ordering) -> Item;
+
+    /// Subtracts `value` from the currently-stored value, wrapping on
+    /// underflow, and stores the result in `self`.
+    ///
+    /// Returns the previously-stored value.
+    ///
+    /// Ordering arguments are ignored by non-atomic types.
+    ///
+    /// See also: [`AtomicU8::fetch_sub`].
+    ///
+    /// [`AtomicU8::fetch_sub`]: core::sync::atomic::AtomicU8::fetch_sub
+    fn fetch_sub(&self, value: Item, order: Ordering) -> Item;
+}
+
+macro_rules! radium {
+    ( atom $base:ty, $atom:ty ) => {
+
+        fn new(value: $base) -> Self {
+            <$atom>::new(value)
+        }
+
+        fn fence(order: Ordering) {
+            atomic::fence(order);
+        }
+
+        fn get_mut(&mut self) -> &mut $base {
+            self.get_mut()
+        }
+
+        fn into_inner(self) -> $base {
+            self.into_inner()
+        }
+
+        fn load(&self, order: Ordering) -> $base {
+            self.load(order)
+        }
+
+        fn store(&self, value: $base, order: Ordering) {
+            self.store(value, order);
+        }
+
+        fn swap(&self, value: $base, order: Ordering) -> $base {
+            self.swap(value, order)
+        }
+
+        fn compare_and_swap(
+            &self,
+            current: $base,
+            new: $base,
+            order: Ordering,
+        ) -> $base {
+            self.compare_and_swap(current, new, order)
+        }
+
+        fn compare_exchange(
+            &self,
+            current: $base,
+            new: $base,
+            success: Ordering,
+            failure: Ordering,
+        ) -> Result<$base, $base> {
+            self.compare_exchange(current, new, success, failure)
+        }
+
+        fn compare_exchange_weak(
+            &self,
+            current: $base,
+            new: $base,
+            success: Ordering,
+            failure: Ordering,
+        ) -> Result<$base, $base> {
+            self.compare_exchange_weak(current, new, success, failure)
+        }
+
+    };
+
+    ( atom_bits $base:ty, $atom:ty ) => {
+        fn fetch_and(&self, value: $base, order: Ordering) -> $base {
+            self.fetch_and(value, order)
+        }
+
+        fn fetch_nand(&self, value: $base, order: Ordering) -> $base {
+            self.fetch_nand(value, order)
+        }
+
+        fn fetch_or(&self, value: $base, order: Ordering) -> $base {
+            self.fetch_or(value, order)
+        }
+
+        fn fetch_xor(&self, value: $base, order: Ordering) -> $base {
+            self.fetch_xor(value, order)
+        }
+    };
+
+    ( atom_math $base:ty, $atom:ty ) => {
+        fn fetch_add(&self, value: $base, order: Ordering) -> $base {
+            self.fetch_add(value, order)
+        }
+
+        fn fetch_sub(&self, value: $base, order: Ordering) -> $base {
+            self.fetch_sub(value, order)
+        }
+    };
+
+    ( bits $( $base:ty , $atom:ty );* ) => { $(
+        impl Radium<$base> for $atom {
+            radium!(atom $base, $atom);
+        }
+
+        impl Radium<$base> for Cell<$base> {
+            radium!(cell $base);
+        }
+
+        impl RadiumBits<$base> for $atom {
+            radium!(atom_bits $base, $atom);
+        }
+
+        impl RadiumBits<$base> for Cell<$base> {
+            radium!(cell_bits $base);
+        }
+    )* };
+
+    ( cell $base:ty ) => {
+
+        fn new(value: $base) -> Self {
+            Cell::new(value)
+        }
+
+        fn fence(_: Ordering) {}
+
+        fn get_mut(&mut self) -> &mut $base {
+            self.get_mut()
+        }
+
+        fn into_inner(self) -> $base {
+            self.into_inner()
+        }
+
+        fn load(&self, _: Ordering) -> $base {
+            self.get()
+        }
+
+        fn store(&self, value: $base, _: Ordering) {
+            self.set(value);
+        }
+
+        fn swap(&self, value: $base, _: Ordering) -> $base {
+            self.replace(value)
+        }
+
+        fn compare_and_swap(
+            &self,
+            current: $base,
+            new: $base,
+            _: Ordering,
+        ) -> $base {
+            if self.get() == current {
+                self.replace(new)
+            }
+            else {
+                self.get()
+            }
+        }
+
+        fn compare_exchange(
+            &self,
+            current: $base,
+            new: $base,
+            _: Ordering,
+            _: Ordering,
+        ) -> Result<$base, $base> {
+            if self.get() == current {
+                Ok(self.replace(new))
+            }
+            else {
+                Err(self.get())
+            }
+        }
+
+        fn compare_exchange_weak(
+            &self,
+            current: $base,
+            new: $base,
+            success: Ordering,
+            failure: Ordering,
+        ) -> Result<$base, $base> {
+            Radium::compare_exchange(self, current, new, success, failure)
+        }
+
+    };
+
+    ( cell_bits $base:ty ) => {
+        fn fetch_and(&self, value: $base, _: Ordering) -> $base {
+            self.replace(self.get() & value)
+        }
+
+        fn fetch_nand(&self, value: $base, _: Ordering) -> $base {
+            self.replace(!(self.get() & value))
+        }
+
+        fn fetch_or(&self, value: $base, _: Ordering) -> $base {
+            self.replace(self.get() | value)
+        }
+
+        fn fetch_xor(&self, value: $base, _: Ordering) -> $base {
+            self.replace(self.get() ^ value)
+        }
+    };
+
+    ( cell_math $base:ty ) => {
+        fn fetch_add(&self, value: $base, _: Ordering) -> $base {
+            self.replace(self.get().wrapping_add(value))
+        }
+
+        fn fetch_sub(&self, value: $base, _: Ordering) -> $base {
+            self.replace(self.get().wrapping_sub(value))
+        }
+    };
+
+    ( num $( $base:ty , $atom:ty ; )* ) => { $(
+        radium!(bits $base, $atom);
+
+        impl RadiumMath<$base> for $atom {
+            radium!(atom_math $base, $atom);
+        }
+
+        impl RadiumMath<$base> for Cell<$base> {
+            radium!(cell_math $base);
+        }
+        )* };
+
+    ( ptr ) => {
+        impl<T> Radium<*mut T> for AtomicPtr<T> {
+            radium!(atom *mut T, AtomicPtr<T>);
+        }
+
+        impl<T> Radium<*mut T> for Cell<*mut T> {
+            radium!(cell *mut T);
+        }
+    };
+}
+
+radium![
+    num
+    i8, AtomicI8;
+    i16, AtomicI16;
+    i32, AtomicI32;
+    i64, AtomicI64;
+    isize, AtomicIsize;
+    u8, AtomicU8;
+    u16, AtomicU16;
+    u32, AtomicU32;
+    u64, AtomicU64;
+    usize, AtomicUsize;
+];
+
+radium!(bits bool, AtomicBool);
+
+radium!(ptr);
