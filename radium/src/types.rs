f0978d81010f2dd149ef9566cf1e494b3748d3c3
Nika Layzell <nika@thelayzells.com>
2020-10-25 15:21:12 -0400

    Use if_atomic! for RadiumT type aliases

--- radium/src/types.rs
+++ radium/src/types.rs
@@ -5,50 +5,74 @@
 //! be used as a consistent name for crates that need consistent names to
 //! portable types.
 
-macro_rules! radium_type {
-    ($flag:ident $(
-        $name:ident $atom:ident => $inner:ty => $adoc:literal $cdoc:literal
-    )*) => { $(
-        #[doc = $adoc]
-        #[cfg($flag)]
-        pub type $name = core::sync::atomic::$atom;
+/// Best-effort atomic bool type
+pub type RadiumBool = if_atomic! {
+    if atomic(bool) { core::sync::atomic::AtomicBool }
+    else { core::cell::Cell<bool> }
+};
 
-        #[doc = $cdoc]
-        #[cfg(not($flag))]
-        pub type $name = core::cell::Cell<$inner>;
-    )* };
-}
+/// Best-effort atomic u8 type
+pub type RadiumU8 = if_atomic! {
+    if atomic(8) { core::sync::atomic::AtomicU8 }
+    else { core::cell::Cell<u8> }
+};
 
-radium_type!(radium_atomic_8
-    RadiumBool AtomicBool => bool => "`AtomicBool`" "`Cell<bool>`"
-    RadiumI8 AtomicI8 => i8 => "`AtomicI8`" "`Cell<i8>`"
-    RadiumU8 AtomicU8 => u8 => "`AtomicU8`" "`Cell<u8>`"
-);
+/// Best-effort atomic i8 type
+pub type RadiumI8 = if_atomic! {
+    if atomic(8) { core::sync::atomic::AtomicI8 }
+    else { core::cell::Cell<i8> }
+};
 
-radium_type!(radium_atomic_16
-    RadiumI16 AtomicI16 => i16 => "`AtomicI16`" "`Cell<i16>`"
-    RadiumU16 AtomicU16 => u16 => "`AtomicU16`" "`Cell<u16>`"
-);
+/// Best-effort atomic u16 type
+pub type RadiumU16 = if_atomic! {
+    if atomic(16) { core::sync::atomic::AtomicU16 }
+    else { core::cell::Cell<u16> }
+};
 
-radium_type!(radium_atomic_32
-    RadiumI32 AtomicI32 => i32 => "`AtomicI32`" "`Cell<i32>`"
-    RadiumU32 AtomicU32 => u32 => "`AtomicU32`" "`Cell<u32>`"
-);
+/// Best-effort atomic i16 type
+pub type RadiumI16 = if_atomic! {
+    if atomic(16) { core::sync::atomic::AtomicI16 }
+    else { core::cell::Cell<i16> }
+};
 
-radium_type!(radium_atomic_64
-    RadiumI64 AtomicI64 => i64 => "`AtomicI64`" "`Cell<i64>`"
-    RadiumU64 AtomicU64 => u64 => "`AtomicU64`" "`Cell<u64>`"
-);
+/// Best-effort atomic u32 type
+pub type RadiumU32 = if_atomic! {
+    if atomic(32) { core::sync::atomic::AtomicU32 }
+    else { core::cell::Cell<u32> }
+};
 
-radium_type!(radium_atomic_ptr
-    RadiumIsize AtomicIsize => isize => "`AtomicIsize`" "`Cell<isize>`"
-    RadiumUsize AtomicUsize => usize => "`AtomicUsize`" "`Cell<usize>`"
-);
+/// Best-effort atomic i32 type
+pub type RadiumI32 = if_atomic! {
+    if atomic(32) { core::sync::atomic::AtomicI32 }
+    else { core::cell::Cell<i32> }
+};
 
-/// `AtomicPtr`
-#[cfg(radium_atomic_ptr)]
-pub type RadiumPtr<T> = core::sync::atomic::AtomicPtr<T>;
+/// Best-effort atomic u64 type
+pub type RadiumU64 = if_atomic! {
+    if atomic(64) { core::sync::atomic::AtomicU64 }
+    else { core::cell::Cell<u64> }
+};
 
-/// `Cell<*mut T>`
-#[cfg(not(radium_atomic_ptr))]
-pub type RadiumPtr<T> = core::cell::Cell<*mut T>;
+/// Best-effort atomic i64 type
+pub type RadiumI64 = if_atomic! {
+    if atomic(64) { core::sync::atomic::AtomicI64 }
+    else { core::cell::Cell<i64> }
+};
+
+/// Best-effort atomic usize type
+pub type RadiumUsize = if_atomic! {
+    if atomic(size) { core::sync::atomic::AtomicUsize }
+    else { core::cell::Cell<usize> }
+};
+
+/// Best-effort atomic isize type
+pub type RadiumIsize = if_atomic! {
+    if atomic(size) { core::sync::atomic::AtomicIsize }
+    else { core::cell::Cell<isize> }
+};
+
+/// Best-effort atomic pointer type
+pub type RadiumPtr<T> = if_atomic! {
+    if atomic(size) { core::sync::atomic::AtomicPtr<T> }
+    else { core::cell::Cell<*mut T> }
+};
